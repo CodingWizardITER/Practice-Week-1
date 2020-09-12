b1000c179151d3aba2b0f619c103c00c6b5423eb
CodingWizardITER <70839355+CodingWizardITER@users.noreply.github.com>
2020-09-12 15:04:15 +0700

    Update Staircase.py

--- Python Solutions/Staircase.py
+++ Python Solutions/Staircase.py
@@ -9,7 +9,7 @@ import sys
 # Complete the staircase function below.
 def staircase(n):
     for i in range(1, n + 1):
-       print(' ' * (n - i) + '#' * i)     // prints (n - i) white spaces, and i-th #s, where, i = row no.
+       print(' ' * (n - i) + '#' * i)     # prints (n - i) white spaces, and i-th #s, where, i = row no.
             
 if __name__ == '__main__':
     n = int(input())
